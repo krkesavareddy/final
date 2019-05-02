a49adf571a497d9ec7e36127acfab655f7132322
techfreks <48522802+techfreks@users.noreply.github.com>
2019-05-02 19:34:36 +0530

    update controller

--- src/main/java/com/hcl/retailbank/controller/RetailController.java
+++ src/main/java/com/hcl/retailbank/controller/RetailController.java
@@ -1,6 +1,7 @@
 package com.hcl.retailbank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.hcl.retailbank.service.RetailbankService;
 public class RetailController{
 	@Autowired
 	RetailbankService retailbankService;
+	@PostMapping("/fundtransfer")
 public FundTransferResponce fundTransfer(@RequestBody FundTransferRequest fundTransferrequest ) {
 	return retailbankService.fundTransfer( fundTransferrequest );
 
